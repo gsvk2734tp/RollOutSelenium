3ccca3ffb007039f5b972ab7a58d324d455b44a8
gsvk2734tp <gsvk2734tp@gmail.com>
2018-04-03 20:55:29 +0300

    правки

--- src/test/java/RollOut/RollOutWeb.java
+++ src/test/java/RollOut/RollOutWeb.java
@@ -34,7 +34,7 @@ public abstract class RollOutWeb {
     @Parameterized.Parameters
     public static List<Object> data() {
         options.setBinary(operaPath);
-        Object[] data = new Object[]{new EdgeDriver()};
+        Object[] data = new Object[]{new ChromeDriver(), new FirefoxDriver(), new OperaDriver(options), new EdgeDriver()};
         return Arrays.asList(data);
     }
 
