a4c70c0991a3b8a5a77ea3b004264022253246d6
gsvk2734tp <gsvk2734tp@gmail.com>
2018-05-04 18:21:39 +0300

    Opera удалена из поддержки   weqwe

--- src/main/java/RegEx.java
+++ src/main/java/RegEx.java
@@ -1,6 +1,6 @@
 public class RegEx {
     public static void main(String[] args) {
-        String testLine1 = "+7921231231233132423154131";
+        String testLine1 = "+7921231231233131`2423154131";
         String testLine2 = "+23";
         System.out.println(testLine2.replaceAll("^[+0-9][0-9]{2,15}$", ""));
     }
